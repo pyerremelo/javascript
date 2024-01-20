11df650df6aae2bae947c10d550c5addc6e331eb
pyerremelo <melopyerre@gmail.com>
2024-01-20 14:34:47 -0300

    Criado o ex008

---
+++ guanabara/aula-11/ex008.js
@@ -0,0 +1,10 @@
+// console.log('O console funcionou corretamente')
+
+
+var vel = 67
+
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 60) {
+    console.log('Você foi multado por excesso de velocidade =(')
+
+}
