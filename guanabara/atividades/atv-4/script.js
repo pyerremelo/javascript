b7c9255ee06f16ea1c57120cceecb712aca2f149
pyerremelo <melopyerre@gmail.com>
2024-02-01 09:14:40 -0300

    Corrigido alguns probleminhas

--- guanabara/atividades/atv-4/script.js
+++ guanabara/atividades/atv-4/script.js
@@ -17,39 +17,49 @@ function calcular() {
         res.innerHTML = '' // Limpa a tela
         res.setAttribute('size', 0)
 
-    } else if (inicio > 0 && limite > 0 && inicio > limite || inicio < 0 && limite < 0 && inicio < limite) {
-        // Caso apareça uma possibilidade impossível, como a descrita acima, aparecerá uma mensagem de erro
-
-        alert('O número inicial da tabuada não pode ser maior que o limite. O inverso é possível. Por favor mude os valores.')
-        res.innerHTML = '' // Limpa a tela
-        res.setAttribute('size', 0)
-
     } else {
-        // Se as condições forem atendidas, aparecerá a tabuada na tag SELECT
+        // Caso os inputs contenham algum valor, entrará nessa parte
 
-        let i = 1 // Contador para 'valor1', 'valor2', ... (importante para o PHP)
-        res.innerHTML = '' // Limpa a tela
         num = Number(num)
         inicio = Number(inicio)
         limite = Number(limite)
 
-        for (inicio; inicio <= limite; inicio++) {
-            // Aqui é onde é realizada a conta e armazenado o valor na variável "item" que é uma tag "OPTION"
-            var item = document.createElement('option')
-            item.text = `${num} x ${inicio} = ${num * inicio}`
-            item.value = `valor${i}` // Contador para 'valor1', 'valor2', ... (importante para o PHP)
-            res.appendChild(item)
-            i++
-        }
+        if (inicio > 0 && limite > 0 && inicio > limite || inicio < 0 && limite < 0 && inicio > limite || inicio > 0 && limite < 0) {
+            // Verificação de impossibilidade de calculo de tabuada conforme o descrito no IF, então aparecerá uma mensagem de erro
 
-        // Essa parte de baixo é para aparecer no SELECT somente os espaços necessários para aparecer todos os itens
-        inicio = document.querySelector('input#inicio')
-        inicio = inicio.value
-        if (inicio > 0) {
-            inicio = Math.abs(inicio)
+            alert('O número inicial da tabuada não pode ser maior que o limite. O inverso é possível. Por favor mude os valores.')
+            res.innerHTML = '' // Limpa a tela
+            res.setAttribute('size', 0)
+
+        } else {
+             // Se as condições forem atendidas e for possível de calcular, aparecerá a tabuada na tag SELECT
+
+            let i = 1 // Contador para 'valor1', 'valor2', ... (importante para o PHP)
+            res.innerHTML = '' // Limpa a tela
+
+            for (inicio; inicio <= limite; inicio++) {
+                // Aqui é onde é realizada a conta e armazenado o valor na variável "item" que é uma tag "OPTION"
+                var item = document.createElement('option')
+                item.text = `${num} x ${inicio} = ${num * inicio}`
+                item.value = `valor${i}` // Contador para 'valor1', 'valor2', ... (importante para o PHP)
+                res.appendChild(item)
+                i++
+            }
+
+            // Essa parte de baixo é para aparecer no SELECT somente os espaços necessários para aparecer todos os itens
+            inicio = document.querySelector('input#inicio')
+            inicio = Number(inicio.value)
+            if (inicio >= 0) {
+                inicio = Math.abs(inicio)
+                limite = Math.abs(limite)
+                var size = limite - inicio + 1
+            } else {
+                inicio = Math.abs(inicio)
+                limite = Math.abs(limite)
+                size = inicio - limite + 1   
+            }
+            
+            res.setAttribute('size', size)
         }
-        limite = Math.abs(limite)
-        let size = limite - inicio + 1
-        res.setAttribute('size', size)
-    }
+    }  
 }
